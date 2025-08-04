15dab9cc64045bab215a0a47aad7858cbd532c59
swetanjana <maiteeswetanjana@gmail.com>
2025-08-05 01:26:49 +0530

    project_section_done

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import Card from '../Card/Card';
 import "./Project.css"
+import simon from '../../assets/simonsays.jpg';
+import spotify from '../../assets/spotify.jpg';
+import photo from '../../assets/photo.jpg';
+import login from '../../assets/loginpage.png';
+import tourism from '../../assets/tour.jpg';
 function Project() {
     return (
         <div id="projects">
-            <h1>My Projects</h1>
+            <h1 id='para'>MY PROJECTS</h1>
             <div className="slider">
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
+                <Card title="Simon Says Game" image={simon}/>
+                <Card title="Spotify Clone" image={spotify}/>
+                <Card title="Photography Portfolio" image={photo}/>
+                <Card title="Login Page" image={login}/>
+                <Card title="Tourism" image={tourism}/>
             </div>
         </div>
     );
